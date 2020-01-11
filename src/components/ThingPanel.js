f4aff537025393443f0d53cdbcaf4dac984afa01
Hinata <hinataizayoi@gmail.com>
2020-01-11 03:25:31 -0600

    Download count

--- src/components/ThingPanel.js
+++ src/components/ThingPanel.js
@@ -11,6 +11,7 @@ export default class ThingOverview extends React.Component {
     this.unlike = this.unlike.bind(this);
     this.bookmark = this.bookmark.bind(this);
     this.unBookmark = this.unBookmark.bind(this);
+    this.download = this.download.bind(this);
   }
 
   async like(e) {
@@ -33,8 +34,12 @@ export default class ThingOverview extends React.Component {
     await this.props.unBookmark();
   }
 
+  async download() {
+    await this.props.download();
+  }
+
   render() {
-    const {likeCount, bookmarkCount, makeCount, remixCount, liked, bookmarked, downloadLink} = this.props;
+    const {likeCount, bookmarkCount, downloadCount, makeCount, remixCount, liked, bookmarked, downloadLink} = this.props;
 
     return <div>
       {!liked && <span class="Mend(26px) Cur(p)" onClick={this.like}><i class="fas fa-thumbs-up"></i> {likeCount}</span>}
@@ -43,7 +48,7 @@ export default class ThingOverview extends React.Component {
       {!bookmarked && <span class="Mend(26px) Cur(p)" onClick={this.bookmark}><i class="fas fa-bookmark"></i> {bookmarkCount}</span>}
       {bookmarked && <span class="Mend(26px) Cur(p) C(#0280ae)" onClick={this.unBookmark}><i class="fas fa-bookmark"></i> {bookmarkCount}</span>}
 
-      <a href={downloadLink} class="Mend(26px) Cur(p) C(black) Td(n):h" onClick={this.download}><i class="fas fa-download"></i></a>
+      <a href={downloadLink} class="Mend(26px) Cur(p) C(black) Td(n):h" onClick={this.download}><i class="fas fa-download"></i> {downloadCount}</a>
       <Link to="/things/new/make" class="Td(n):h Fz(16px) C(black) Mend(26px) Cur(p)"><i class="fas fa-wrench"></i> MAKE {makeCount}</Link>
       <Link to="/things/new/remix"class="Td(n):h Fz(16px) C(black) Cur(p)"><i class="fas fa-compact-disc"></i> REMIX {remixCount}</Link>
     </div>
